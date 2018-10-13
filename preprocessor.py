32b4ae57cfa111b98c0780dab8d452b37b765c5e
PankajB1997 <pankajbhootra@gmail.com>
2018-10-13 16:00:30 +0800

    Add scripts to clean/preprocess raw dataset

---
+++ preprocessor.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import csv
+import pickle
+from pprint import pprint
+from stopwords import stopwords
+import unicodedata
+import sys
+
+tbl = dict.fromkeys(i for i in range(sys.maxunicode)
+                      if unicodedata.category(chr(i)).startswith('P'))
+
+def removePunctuations(sentence):
+    return sentence.translate(tbl)
+
+def removeStopwords(sentence):
+    filtered_sentence = ""
+    for word in sentence.split(" "):
+        if word not in stopwords:
+            filtered_sentence += word + " "
+    return filtered_sentence.strip()
+
+def retainAsciiOnly(sentence):
+    filtered_sentence = ""
+    for c in sentence:
+        if ord(c) < 128:
+            filtered_sentence += c
+    return filtered_sentence
+
+def cleanSentence(sentence):
+    sentence = sentence.strip().lower()
+    sentence = retainAsciiOnly(sentence)
+    sentence = removeStopwords(sentence)
+    sentence = removePunctuations(sentence)
+    return sentence
+
+data = pickle.load(open(os.path.join('news', 'data.pkl'), 'rb'))
+titles = data[0]
+articles = data[1]
+
+titles_filtered = []
+articles_filtered = []
+
+for title in titles:
+    titles_filtered.append(cleanSentence(title))
+
+for article in articles:
+    articles_filtered.append(cleanSentence(article))
+
+pickle.dump([ titles_filtered, articles_filtered ], open(os.path.join('news', 'data_filtered.pkl'), 'wb'))
--- text_summarizer.py
+++ text_summarizer.py
@@ -3,35 +3,3 @@ import sys
 import csv
 import pickle
 from pprint import pprint
-
-maxInt = sys.maxsize
-decrement = True
-
-while decrement:
-    # decrease the maxInt value by factor 10
-    # as long as the OverflowError occurs.
-
-    decrement = False
-    try:
-        csv.field_size_limit(maxInt)
-    except OverflowError:
-        maxInt = int(maxInt/10)
-        decrement = True
-
-csvs = [ 'articles1.csv', 'articles2.csv', 'articles3.csv' ]
-titles = []
-articles = []
-
-for file in csvs:
-    with open(os.path.join('news', file), encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            titles.append(row['title'])
-            articles.append(row['content'])
-
-print(titles[0])
-print(articles[0])
-print(len(titles))
-print(len(articles))
-
-pickle.dump([ titles, articles ], open(os.path.join('news', 'data.pkl'), 'wb'))
